0f9aae58a34d316da7c3235226fe03fbe7166a69
Nicholas Ikechukwu <ncholas@bu.edu>
2024-09-26 18:58:36 -0400

    wip: next is lean4 code

--- cs511HwSolutions/hw03/hw03_nicholas_ikechukwu.lean
+++ cs511HwSolutions/hw03/hw03_nicholas_ikechukwu.lean
@@ -3,105 +3,44 @@ import Library.Basic
 
 math2001_init
 
-/-! # Homework 2 -/
+-- # Exercise 3
 
-/- # Exercise 3 -/
+/-2 points-/
+theorem exercise2_3_6_2 {x : ℝ} (h : x = 1 ∨ x = 2) : x ^ 2 - 3 * x + 2 = 0 := by
+  sorry
 
--- Example 1.4.6
-example {n : ℤ} (hn : n ≥ 5) : n ^ 2 > 2 * n + 11 :=
-  calc
-    n ^ 2 = n * n := by ring
-    _ ≥ 5 * n := by rel [hn]
-    _ = 2 * n + 3 * n := by ring
-    _ ≥ 2 * n + 3 * 5 := by rel [hn]
-    _ = 2 * n + 11 + 4 := by ring
-    _ > 2 * n + 11 := by extra
+/-2 points-/
+theorem exercise2_3_6_3 {t : ℚ} (h : t = -2 ∨ t = 3) : t ^ 2 - t - 6 = 0 := by
+  sorry
 
+/-2 points-/
+theorem exercise2_3_6_4 {x y : ℝ} (h : x = 2 ∨ y = -2) : x * y + 2 * x = 2 * y + 4 := by
+  sorry
 
--- Example 2.1.3
-example {r s : ℚ} (h1 : s + 3 ≥ r) (h2 : s + r ≤ 3) : r ≤ 3 :=
-  have h3 : r ≤ 3 + s := by addarith [h1]
-  have h4 : r ≤ 3 - s := by addarith [h2]
-  calc
-    r = (r + r) / 2 := by ring
-    _ ≤ (3 - s + (3 + s)) / 2 := by rel [h3, h4]
-    _ = 3 := by ring
+-- # Exercise 4
 
--- Example 2.1.7
-example (a b : ℝ) (h1 : -b ≤ a) (h2 : a ≤ b) : a ^ 2 ≤ b ^ 2 :=
-  have h3: 0 ≤ b + a := by addarith [h1]
-  have h4: 0 ≤ b - a := by addarith [h2]
-  calc
-    a ^ 2 ≤ a ^ 2 + (b + a)*(b - a) := by extra
-    _ = a ^ 2 + b ^ 2 - a ^ 2 := by ring
-    _ = b ^ 2 := by ring
+/-2 points-/
+theorem exercise2_3_6_12 {x : ℤ} : 2 * x ≠ 3 := by
+  sorry
 
+/-2 points-/
+theorem exercise2_4_5_1 {a b : ℚ} (H : a ≤ 1 ∧ a + b ≤ 3) : 2 * a + b ≤ 4 := by
+  sorry
 
+/-2 points-/
+theorem exercise2_4_5_6 {x y : ℚ} (h : x + y = 5 ∧ x + 2 * y = 7) : x = 3 ∧ y = 2 := by
+  sorry
 
-/- # Exercise 4 -/
+-- # Problem 2
 
+/-2 points-/
+theorem exercise2_3_6_10 {t : ℝ} (ht : t ^ 3 = t ^ 2) : t = 1 ∨ t = 0 := by
+  sorry
 
+/-2 points-/
+theorem exercise2_3_6_14 {m : ℕ} : m ^ 2 + 4 * m ≠ 46 := by
+  sorry
 
--- example {x : ℚ} (h1 : x ^ 2 = 4) (h2 : 1 < x) : x = 2 :=
---   have h3:=
---   calc
---     x * x = x ^ 2 := by ring
---     _ = 4 := by rw [h1]
---     _ = 4 + 2 * x - 2 * x:= by ring
---     _ = 2 * 2 + 2 * x - 2 * x := by ring
---     _ = 2 * (x + 2) := by ring
---   cancel x at h3
---   addarith [h3]
-
--- Exercise 2.1.9 (1)
--- example {x : ℚ} (h1 : x ^ 2 = 4) (h2 : 1 < x) : x = 2 :=
---   have h3:=
---   calc
---     x * (x + 2) = 4 + 2 * x := by extra
---     _ = 2 * 2 + 2 * x := by ring
---     _ = 2 * (x + 2) := by ring
---   cancel x at h3
---   addarith [h3]
-
-
--- Exercise 2.1.9 (1)
-example {x : ℚ} (h1 : x ^ 2 = 4) (h2 : 1 < x) : x = 2 := by sorry
-
--- Exercise 2.1.9 (3)
-example (x y : ℚ) (h : x * y = 1) (h2 : x ≥ 1) : y ≤ 1 := by sorry
-
---Exercise 2.2.4 (1)
-example {m : ℤ} (hm : m + 1 = 5) : 3 * m ≠ 6 := by sorry
-
-/- # Problem 2 -/
-
--- -- Example 2.1.8
--- example (a b : ℝ) (h : a ≤ b) : a ^ 3 ≤ b ^ 3 :=
---   have h1: 0 ≤ b - a := by addarith [h]
---   have h2 : 0 ≤ (b - a) ^ 2 := by extra
---   have h3 : 0 ≤ (b + a) ^ 2 := by extra
---   calc
---     a ^ 3 ≤ a ^ 3 + ((b - a) * ((b - a)^2 + 3* (b + a)^2)) / 4 := by extra
---     _ ≤  a
---     _ ≤ b ^ 3 := by rel [h1, h2]
-
--- Example 2.1.8
--- example (a b : ℝ) (h : a ≤ b) : a ^ 3 ≤ b ^ 3 :=
---   have h3 :=
---   calc
---     a ^ 3 ≤ a ^ 3 + ((b - a) * ((b - a)^2 + 3* (b + a)^2)) / 4 := by ring
---     _ ≤ b ^ 3 := by rel [h3]
-
-
--- Exercise 2.1.9 (2)
-example {n : ℤ} (hn : n ^ 2 + 4 = 4 * n) : n = 2 := by sorry
-
-
-
-
--- Exercise 2.2.4 (2)
-example {s : ℚ} (h1 : 3 * s ≤ -6) (h2 : 2 * s ≥ -4) : s = -2 := by
-  apply le_antisymm
-  calc
-    s = (2 * s) / 2 := by ring
-    _ ≤ -4  / 2 := by rw [h1]
+/-2 points-/
+theorem exercise2_4_5_7 {a b : ℝ} (h1 : a * b = a) (h2 : a * b = b) : a = 0 ∧ b = 0 ∨ a = 1 ∧ b = 1 := by
+  sorry
