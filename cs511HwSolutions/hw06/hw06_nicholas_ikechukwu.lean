2b698e268377ea7259c6989181c70677a0333c28
Nicholas Ikechukwu <ncholas@bu.edu>
2024-10-20 22:17:49 -0400

    finalizing hw06

--- cs511HwSolutions/hw06/hw06_nicholas_ikechukwu.lean
+++ cs511HwSolutions/hw06/hw06_nicholas_ikechukwu.lean
@@ -6,49 +6,40 @@ import Library.Theory.ParityModular
 import AutograderLib
 
 math2001_init
-
 --# Exercise 3.4.5.6
 
 @[autograded 3]
-theorem exercise_3a (n : ℤ) : 5 * n ^ 2 + 3 * n + 7 ≡  1 [ZMOD 2] := by
-  calc
-    5 * n ^ 2 + 3 * n + 7 ≡ n ^ 2 + n + 1 [ZMOD 2] := by rel []
-    _ = n * (n + 1) + 1 := by ring
-    _ ≡ 0 + 1 [ZMOD 2] := by rel [Int.modEq_zero_fac']
-    _ = 1 := by ring
+theorem exercise_3a (n : ℤ) : 5 * n ^ 2 + 3 * n + 7 ≡ 1 [ZMOD 2] := by
+  rel [Int.ModEq.refl]
 
 --# Exercise 3.4.5.7
 
 @[autograded 3]
-theorem exercise_3b {x : ℤ} : x ^ 5 ≡  x [ZMOD 5] := by
-  mod_cases hx : x % 5
-  all_goals
-    calc
-      x ^ 5 ≡ (x % 5) ^ 5 [ZMOD 5] := by rel [hx]
-      _ = x % 5 := by numbers
-      _ ≡ x [ZMOD 5] := by rel [hx]
+theorem exercise_3b {x : ℤ} : x ^ 5 ≡ x [ZMOD 5] := by
+  have h : x ^ 5 - x = x * (x ^ 4 - 1) := by ring
+  have h' : x ^ 4 - 1 = (x ^ 2 - 1) * (x ^ 2 + 1) := by ring
+  have h'' : x ^ 2 - 1 = (x - 1) * (x + 1) := by ring
+  rw [h, h', h'']
+  apply Int.ModEq.mul_right
+  apply Int.ModEq.mul
+  · apply Int.modEq_zero_fac
+  · apply Int.modEq_zero_fac
 
 --# Exercise 4.4.6.2
 
 @[autograded 3]
 theorem exercise_4a (n : ℤ) (hn : n ^ 2 ≡ 4 [ZMOD 5]) : n ≡ 2 [ZMOD 5] ∨ n ≡ 3 [ZMOD 5] := by
-  mod_cases hn' : n % 5
-  · have h := calc
-      0 ≡ n [ZMOD 5] := by rel [hn']
-      _ ^ 2 ≡ 4 [ZMOD 5] := by rel [hn]
-    numbers at h
-  · have h := calc
-      1 ^ 2 ≡ n ^ 2 [ZMOD 5] := by rel [hn']
-      _ ≡ 4 [ZMOD 5] := by rel [hn]
-    numbers at h
-  · right
-    exact hn'
-  · left
-    exact hn'
-  · have h := calc
-      4 ^ 2 ≡ n ^ 2 [ZMOD 5] := by rel [hn']
-      _ ≡ 4 [ZMOD 5] := by rel [hn]
-    numbers at h
+  have h : (n - 2) * (n - 3) ≡ 0 [ZMOD 5] := by
+    calc (n - 2) * (n - 3)
+      ≡ n ^ 2 - 5 * n + 6 [ZMOD 5] := by rel [Int.ModEq.refl]
+      ≡ 4 - 0 + 1 [ZMOD 5] := by rel [hn, Int.ModEq.refl]
+      ≡ 0 [ZMOD 5] := by rel [Int.ModEq.refl]
+  obtain ⟨k, hk⟩ := h
+  have : n - 2 ≡ 0 [ZMOD 5] ∨ n - 3 ≡ 0 [ZMOD 5] := by
+    apply Int.ModEq.mul_eq_zero hk
+  cases this with
+  | inl h => right; rel [h]
+  | inr h => left; rel [h]
 
 --# Exercise 4.4.6.3
 
@@ -57,11 +48,8 @@ theorem exercise_4b : Prime 7 := by
   apply prime_test
   · numbers
   · intros m hm1 hm2
-    apply Nat.not_dvd_of_exists_lt_and_lt
     interval_cases m
-    all_goals
-      use 1
-      constructor <;> numbers
+    all_goals numbers
 
 --# Example 4.5.4
 
@@ -75,42 +63,15 @@ theorem problem_2a : ¬ (∃ n : ℕ, n ^ 2 = 2) := by
     ring
   have h_odd : Int.Odd n ∨ Int.Even n := Int.odd_or_even n
   cases h_odd with
-  | inl h_odd =>
-    have h_odd_sq : Int.Odd (n ^ 2) := by
-      apply Int.odd_sq
-      exact h_odd
-    apply no_int_even_and_odd
-    use n ^ 2
-    constructor
-    · exact h_even
-    · exact h_odd_sq
-  | inr h_even =>
-    have h_even_sq : Int.Even (n ^ 2) := by
-      apply Int.even_sq
-      exact h_even
-    have h_even_2 : Int.Even 2 := by
-      rw [← hn]
-      exact h_even_sq
-    have h_odd_2 : Int.Odd 2 := by
-      use 1
-      ring
-    apply no_int_even_and_odd
-    use 2
-    constructor
-    · exact h_even_2
-    · exact h_odd_2
+  | inl h_odd => exact no_int_even_and_odd ⟨n ^ 2, h_even, Int.odd_sq h_odd⟩
+  | inr h_even => exact no_int_even_and_odd ⟨2, by { rw [← hn]; exact Int.even_sq h_even }, by use 1; ring⟩
 
 --# Example 4.5.5
 
 @[autograded 2]
 theorem problem_2b (n : ℤ) : Int.Odd n ↔ ¬ Int.Even n := by
   constructor
-  · intros h1 h2
-    apply no_int_even_and_odd
-    use n
-    constructor
-    · exact h2
-    · exact h1
+  · intro h1 h2; exact no_int_even_and_odd ⟨n, h2, h1⟩
   · intro h
     cases Int.even_or_odd n with
     | inl h1 => contradiction
@@ -119,21 +80,16 @@ theorem problem_2b (n : ℤ) : Int.Odd n ↔ ¬ Int.Even n := by
 --# Example 4.5.6
 
 @[autograded 2]
-theorem problem_2c (n : ℤ) : ¬(n ^ 2 ≡  2 [ZMOD 3]) := by
+theorem problem_2c (n : ℤ) : ¬(n ^ 2 ≡ 2 [ZMOD 3]) := by
   intro h
-  mod_cases hn : n % 3
-  · have h :=
-    calc (0:ℤ) = 0 ^ 2 := by numbers
-      _ ≡ n ^ 2 [ZMOD 3] := by rel [hn]
-      _ ≡ 2 [ZMOD 3] := by rel [h]
-    numbers at h -- contradiction!
-  · have h :=
-    calc (1:ℤ) = 1 ^ 2 := by numbers
-      _ ≡ n ^ 2 [ZMOD 3] := by rel [hn]
-      _ ≡ 2 [ZMOD 3] := by rel [h]
-    numbers at h -- contradiction!
-  · have h :=
-    calc (1:ℤ) = 2 ^ 2 := by numbers
-      _ ≡ n ^ 2 [ZMOD 3] := by rel [hn]
-      _ ≡ 2 [ZMOD 3] := by rel [h]
-    numbers at h -- contradiction!
+  have : n ^ 2 ≡ n [ZMOD 3] := by
+    calc n ^ 2 ≡ n * n [ZMOD 3] := by rel [Int.ModEq.refl]
+               ≡ n [ZMOD 3] := by apply Int.ModEq.mul_right; apply Int.modEq_add_fac_self_symm''
+  have : n ≡ 2 [ZMOD 3] := by
+    calc n ≡ n ^ 2 [ZMOD 3] := by rel [this.symm]
+           ≡ 2 [ZMOD 3] := by rel [h]
+  have : 2 ≡ 2 ^ 2 [ZMOD 3] := by
+    calc 2 ≡ n [ZMOD 3] := by rel [this.symm]
+           ≡ n ^ 2 [ZMOD 3] := by rel [this]
+           ≡ 2 [ZMOD 3] := by rel [h]
+  numbers at this
